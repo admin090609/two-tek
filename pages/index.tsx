d1401f2595437ad4951b2de37d965a887d937689
Loghin Dan <loghindan494@gmail.com>
2023-10-03 20:04:44 +0300

    jksbdv

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div className="mt-20 text-center relative">
-        <div className="w-[60vw] mx-auto relative">
-          <div className="rounded-3xl overflow-hidden" style={{ width: "100%", height: "450px" }}>
+        <div className="w-[70vw] mx-auto relative">
+          <div
+            className="rounded-3xl overflow-hidden"
+            style={{ width: "100%", height: "500px" }}
+          >
             <Image
               src="/images/slider.jpg"
               alt=""
@@ -14,15 +18,72 @@ export default function Home() {
               className="rounded-3xl"
             />
           </div>
-          <div className="absolute top-0 left-0 flex flex-col justify-center items-center text-white w-fit bg-white/25 rounded-3xl mx-16 my-20 text-start">
-            <p className="text-5xl font-bold mb-4 px-10 py-3 leading-tight">
-              Creăm <br /> Profesional <br /> <span className="rounded-[50px] px-3 font-light border-2 border-[#451D75]">WebSite-ul</span> <br />
+          <div className="absolute top-1/2 left-0 transform -translate-y-1/2 flex flex-col justify-center items-center text-white w-fit bg-white/25 rounded-3xl mx-16 text-start">
+            <p className="text-6xl font-bold mb-4 px-10 py-3">
+              Creăm <br /> Profesional <br />{" "}
+              <span className="tracking-wider text-[45px] rounded-[50px] px-3 font-light border-2 border-[#451D75]">
+                WebSite-ul
+              </span>{" "}
+              <br />
               Dumneavoastră
             </p>
-            <button className="px-10 py-5">
-              Contactează-ne la +373 685 487 89
-            </button>
+            <Link href="tel:+37368548789">
+              <button className="px-8 py-2 font-bold mb-5 text-black bg-white rounded-[50px]">
+                Contactează-ne la +373 685 487 89
+              </button>
+            </Link>
           </div>
+        </div>
+      </div>
+      <h1 className="mt-20 text-center text-4xl">
+        Transformăm ideile tale în site-uri <br /> funcționale și atrăgătoare.
+      </h1>
+      <div className="flex justify-evenly items-center mt-12">
+        <div className="w-20 h-20">
+          <Link href="">
+            {" "}
+            <Image
+              width={100}
+              height={100}
+              src="/images/sanduta.png"
+              alt=""
+              className="object-contain w-full h-full"
+            />
+          </Link>
+        </div>
+        <div className="w-20 h-20">
+          <Link href="https://admin090609.github.io/">
+            <Image
+              width={100}
+              height={100}
+              src="/images/buffi.png"
+              alt=""
+              className="object-contain w-full h-full"
+            />
+          </Link>
+        </div>
+        <div className="w-20 h-20">
+          <Link href="https://adfitness.vercel.app/">
+            <Image
+              width={100}
+              height={100}
+              src="/images/a&d.png"
+              alt=""
+              className="object-contain w-full h-full"
+            />
+          </Link>
+        </div>
+        <div className="w-16 h-16">
+          <Link href="https://apisudex.store/">
+            {" "}
+            <Image
+              width={100}
+              height={100}
+              src="/images/apisudex.png"
+              alt=""
+              className="object-contain w-full h-full"
+            />
+          </Link>
         </div>
       </div>
     </>
