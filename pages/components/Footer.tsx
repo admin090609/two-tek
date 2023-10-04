7a0a2e28ac5ec10510159ec50b01afe3ceafbc22
Loghin Dan <loghindan494@gmail.com>
2023-10-04 21:37:40 +0300

    ava

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -5,23 +5,23 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-      <div className="flex justify-evenly items-center">
+      <div className="flex justify-evenly items-start mt-20 mb-0">
         <div>
-          <h1 className="text-5xl fo">2Tek</h1>
+          <h1 className="text-6xl font-bold">2Tek</h1>
         </div>
-        <div>
-          <h1>Pagini</h1>
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-semibold mb-3">Pagini</h1>
           <Link href="">Acasa</Link>
           <Link href="">Acasa</Link>
           <Link href="">Acasa</Link>
           <Link href="">Acasa</Link>
           <Link href="">Acasa</Link>
         </div>
-        <div>
-          <h1>Contacte</h1>
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-semibold mb-3">Contacte</h1>
           <Link href="tel:+37368548789">+373 685 487 89</Link>
           <Link href="tel:+37360008857">+373 600 088 57</Link>
-          <p>Imagetwo2tek@gmail.com</p>
+          <p>two2tek@gmail.com</p>
           <Image src="" alt="" width={100} height={100} />
           <Image src="" alt="" width={100} height={100} />
         </div>
