c12c4e583fc660f1e2f52b6cab01f7ccf2a7cfe4
Loghin Dan <loghindan494@gmail.com>
2023-10-04 20:30:53 +0300

    hkvjav

--- pages/index.tsx
+++ pages/index.tsx
@@ -140,23 +140,27 @@ export default function Home() {
       </div>
 
       <h1 className="text-center text-4xl mb-20">Cum lucrăm noi</h1>
-      <div>
-        <div>
-          <h1>Share your Calendly availability with others</h1>{" "}
-          <p>
+      <div className="flex justify-around items-center mb-5">
+        <div className="max-w-xl">
+          <h1 className="text-4xl font-bold mb-5 leading-normal">
+            Share your Calendly <br /> availability with others
+          </h1>{" "}
+          <p className="text-2xl leading-normal">
             Grow your business with scheduling automation. Simply email, text,
             or add your Calendly availability to your website - and watch
             prospects and recruits book high-value meetings with you.
           </p>
         </div>
         <div>
-          <Image src="" alt="" width={100} height={100} />
+          <Image src="/images/cal.png" alt="" width={500} height={300} />
         </div>
       </div>
-      <div>
-        <div>
-          <h1>Schedule as a team</h1>{" "}
-          <p>
+      <div className="flex justify-around items-center mb-5">
+        <div className="max-w-xl">
+          <h1 className="text-4xl font-bold mb-5 leading-normal">
+            Schedule as a team
+          </h1>{" "}
+          <p className="text-2xl leading-normal">
             Calendly adapts to both you and your team's scheduling preferences.
             Co-host a client call with a colleague, email reminders and
             follow-ups, and integrate everything with your preferred software
@@ -164,20 +168,22 @@ export default function Home() {
           </p>
         </div>
         <div>
-          <Image src="" alt="" width={100} height={100} />
+          <Image src="/images/cal.png" alt="" width={500} height={100} />
         </div>
       </div>
-      <div>
-        <div>
-          <h1>Hit your number</h1>{" "}
-          <p>
+      <div className="flex justify-around items-center mb-5">
+        <div className="max-w-xl">
+          <h1 className="text-4xl font-bold mb-5 leading-normal">
+            Hit your number
+          </h1>{" "}
+          <p className="text-2xl leading-normal">
             High-value meetings are the lifeblood of your business. Increase
             revenue, retain customers, and land recruits with the #1 scheduling
             automation platform.
           </p>
         </div>
         <div>
-          <Image src="" alt="" width={100} height={100} />
+          <Image src="/images/cal.png" alt="" width={500} height={100} />
         </div>
       </div>
     </>
