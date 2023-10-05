8acd71e2b611d4d7c93df2d8d5edd31d6467fe52
Two Tek <two2tek@gmail.com>
2023-10-05 18:38:19 +0300

    asfa

--- pages/components/Slideshow.tsx
+++ pages/components/Slideshow.tsx
@@ -1,16 +1,22 @@
 // components/Slideshow.js
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Slideshow = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const images = ['1.jpg', '4.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+  const images = ["/images/cal.png", "/images/echipa.jpg", "/images/echipa.jpg", "/images/echipa.jpg"];
 
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
   };
 
   useEffect(() => {
@@ -24,19 +30,16 @@ const Slideshow = () => {
         {images.map((image, index) => (
           <div
             key={index}
-            className={`w-32 h-32 bg-cover bg-center rounded-lg ${
-              currentIndex === index ? 'opacity-100' : 'opacity-0'
+            className={`w-64 h-64 bg-cover bg-center rounded-lg ${
+              currentIndex === index ? "opacity-100" : "opacity-0"
             } transition-opacity duration-500`}
             style={{ backgroundImage: `url(${image})` }}
           >
             <div className="absolute inset-0 bg-gray-800 bg-opacity-70 rounded-lg p-4 text-white transition-opacity duration-500">
-              <div className="text-xl font-bold">LUNDEV</div>
-              <div className="text-sm mt-2">
-                Tinh ru anh di chay pho, chua kip chay pho thi anh chay mat tieu
+              <div className="text-5xl font-bold">
+                Proiectele <br /> Noastre
               </div>
-              <button className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 mt-2">
-                See more
-              </button>
+              <div className="text-sm mt-2">Ceva text aici</div>
             </div>
           </div>
         ))}
@@ -44,15 +47,15 @@ const Slideshow = () => {
       <div className="absolute inset-x-0 bottom-4 flex justify-center">
         <button
           onClick={prevSlide}
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-full"
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 p-3 rounded-full"
         >
-          &lt;
+          <Image src="/images/arrow.png" alt="" width={20} height={0} className="rotate-180"/>
         </button>
         <button
           onClick={nextSlide}
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-full ml-4"
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 p-3 rounded-full ml-4"
         >
-          &gt;
+          <Image src="/images/arrow.png" alt="" width={20} height={0} />
         </button>
       </div>
     </div>
