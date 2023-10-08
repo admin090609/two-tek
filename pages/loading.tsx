b65a3c6da3ad682ee00a1c9352bb32b6a9b6eb74
Two Tek <two2tek@gmail.com>
2023-10-08 12:38:06 +0300

    af

---
+++ pages/loading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <>
+      <div className="loader">
+        <li className="ball"></li>
+        <li className="ball"></li>
+        <li className="ball"></li>
+      </div>
+    </>
+  );
+};
+
+export default loading;
