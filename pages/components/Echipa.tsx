28e50793e0da83667c2963a2f3361a09ffd82b26
Loghin Dan <loghindan494@gmail.com>
2023-10-12 22:08:03 +0300

    echipa update

--- pages/components/Echipa.tsx
+++ pages/components/Echipa.tsx
@@ -11,14 +11,22 @@ const Echipa = () => {
           </h1>
           <p>
             Echipa noastră are ca obiectiv să aducă inovație și să sporească
-            eficiența colaborării în colectiv, oferind suport
-            clienților noștri pentru a se dezvolta în mediul digital și a atinge
-            rezultate pe care nu le-au atins până acum. Suntem o companie în
-            plină expansiune, orientată către progres continuu.
+            eficiența colaborării în colectiv, oferind suport clienților noștri
+            pentru a se dezvolta în mediul digital și a atinge rezultate pe care
+            nu le-au atins până acum. Suntem o companie în plină expansiune,
+            orientată către progres continuu.
           </p>
         </div>
-        <div>
-          <Image src="/images/echipa.jpg" alt="" width={600} height={100} />
+        <div className="bg-gray-700 flex justify-center items-center flex-col rounded-[20px]">
+          <Image src="/images/gif.gif" alt="" width={300} height={100} />
+          <div className="flex">
+            <div className="p-4">
+              <Image src="/images/danu.png" alt="" width={250} height={100} />
+            </div>
+            <div className="p-4">
+              <Image src="/images/eduard.png" alt="" width={250} height={100} />
+            </div>
+          </div>
         </div>
       </div>
     </>
