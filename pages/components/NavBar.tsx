bb1a364652861f62836784377e1fba8c806fd64f
Loghin Dan <loghindan494@gmail.com>
2023-10-29 13:32:03 +0200

    update

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -11,14 +11,11 @@ import { getTranslatedContent } from "./TranslateRoToRu";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State for mobile menu
   const router = useRouter();
 
   const { language, setLanguage } = useLanguage();
   const content = getTranslatedContent(language);
-
-  const handleNav = () => {
-    setNav(!nav);
-  };
 
   useEffect(() => {
     // Delay the animation to ensure it plays when the component mounts
@@ -27,6 +24,16 @@ const NavBar = () => {
     }, 500);
   }, []);
 
+  // Toggle the mobile menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // Close the mobile menu
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div
       className={`left-0 top-0 w-full z-10 mt-5 text-black ${
@@ -34,26 +41,47 @@ const NavBar = () => {
       } transition-all duration-1000 ease-in-out`}
     >
       <div
-        className="max-w-[1305px] sm:mt-4 sm:w-full flex justify-around items-center sm:p-0 p-5 h-[70px] rounded-[70px] m-auto"
-        style={{ backgroundImage: "linear-gradient(to bottom, var(--nav_bg1), var(--nav_bg2), var(--nav_bg3))"}}
+        className="max-w-[1305px] sm:mt-4 sm:w-full flex justify-between items-center sm:p-0 p-5 h-[70px] rounded-[70px] m-auto"
+        style={{
+          backgroundImage:
+            "linear-gradient(to bottom, var(--nav_bg1), var(--nav_bg2), var(--nav_bg3))",
+        }}
       >
-        <Link href="/" className="text-4xl sm:p-4 ml-4 font-bold text-focus-in">
+        <Link
+          href="/"
+          className="text-4xl sm:p-4 ml-10 font-bold text-focus-in"
+        >
           2Tek
         </Link>
 
+        {isMenuOpen ? (
+          <AiOutlineClose
+            className="text-4xl sm:p-4 ml-4 cursor-pointer md:block lg:hidden"
+            onClick={closeMenu}
+            size={20}
+          />
+        ) : (
+          <AiOutlineMenu
+            className="text-4xl sm:p-4 ml-4 cursor-pointer md:block lg:hidden"
+            onClick={toggleMenu}
+            size={20}
+          />
+        )}
+
+        {/* Navigation links for PC */}
         <ul
-          className="hidden sm:flex text-base font-semibold text-white "
+          className="hidden md:flex text-base font-semibold text-white"
           style={{ color: "var(--link_color)" }}
         >
           <li className="p-4 relative group">
             <div className="group relative tracking-in-expand-fwd-bottom">
               <ScrollLink
-                to="horizontal" // Identificatorul secțiunii căreia doriți să faceți clic
+                to="horizontal"
                 spy={true}
                 smooth={true}
-                offset={-70} // Ajustați această valoare pentru a potrivi poziția de destinație
+                offset={-70}
                 duration={1000}
-                className="cursor-pointer "
+                className="cursor-pointer"
               >
                 {content.Nav1}
               </ScrollLink>
@@ -66,17 +94,17 @@ const NavBar = () => {
           <li className="p-4 relative group">
             <div className="group relative tracking-in-expand-fwd-bottom">
               <ScrollLink
-                to="echipa" // Identificatorul secțiunii căreia doriți să faceți clic
+                to="echipa"
                 spy={true}
                 smooth={true}
-                offset={-70} // Ajustați această valoare pentru a potrivi poziția de destinație
+                offset={-70}
                 duration={1800}
                 className="cursor-pointer"
               >
                 {content.Nav2}
               </ScrollLink>
               <div
-                className="absolute inset-x-0 bottom-0 h-0.5  transform translate-y-0 scale-x-0 group-hover:scale-x-100 transition-transform origin-bottom"
+                className="absolute inset-x-0 bottom-0 h-0.5 transform translate-y-0 scale-x-0 group-hover:scale-x-100 transition-transform origin-bottom"
                 style={{ background: "var(--link_hover)" }}
               ></div>
             </div>
@@ -84,10 +112,10 @@ const NavBar = () => {
           <li className="p-4 relative group">
             <div className="group relative tracking-in-expand-fwd-bottom">
               <ScrollLink
-                to="proiecte" // Identificatorul secțiunii căreia doriți să faceți clic
+                to="proiecte"
                 spy={true}
                 smooth={true}
-                offset={-70} // Ajustați această valoare pentru a potrivi poziția de destinație
+                offset={-70}
                 duration={1800}
                 className="cursor-pointer"
               >
@@ -102,10 +130,10 @@ const NavBar = () => {
           <li className="p-4 relative group">
             <div className="group relative tracking-in-expand-fwd-bottom">
               <ScrollLink
-                to="feedback" // Identificatorul secțiunii căreia doriți să faceți clic
+                to="feedback"
                 spy={true}
                 smooth={true}
-                offset={-60} // Ajustați această valoare pentru a potrivi poziția de destinație
+                offset={-60}
                 duration={2000}
                 className="cursor-pointer"
               >
@@ -119,74 +147,81 @@ const NavBar = () => {
           </li>
         </ul>
 
-        <div className="flex  items-center">
+        {/* Mobile menu */}
+        {isMenuOpen && (
+          <div className="md:hidden absolute top-0 left-0 right-0 p-5 text-white bg-black z-100">
+            <AiOutlineClose
+              className="text-4xl cursor-pointer absolute top-4 right-4"
+              onClick={closeMenu}
+            />
+            <ul>
+              <li className="my-2">
+                <ScrollLink
+                  to="horizontal"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={1000}
+                  onClick={closeMenu}
+                >
+                  {content.Nav1}
+                </ScrollLink>
+              </li>
+              <li className="my-2">
+                <ScrollLink
+                  to="echipa"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={1800}
+                  onClick={closeMenu}
+                >
+                  {content.Nav2}
+                </ScrollLink>
+              </li>
+              <li className="my-2">
+                <ScrollLink
+                  to="proiecte"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={1800}
+                  onClick={closeMenu}
+                >
+                  {content.Nav3}
+                </ScrollLink>
+              </li>
+              <li className="my-2">
+                <ScrollLink
+                  to="feedback"
+                  spy={true}
+                  smooth={true}
+                  offset={-60}
+                  duration={2000}
+                  onClick={closeMenu}
+                >
+                  {content.Nav4}
+                </ScrollLink>
+              </li>
+              <li className="my-2">
+                <div className="mr-5">
+                  <TranslateRoToRu />
+                </div>
+              </li>
+              <li className="my-2">
+              <Color />
+              </li>
+            </ul>
+          </div>
+        )}
+
+        <div className="lg:flex items-center mr-10 md:hidden sm:hidden">
           <div className="mr-5">
             <TranslateRoToRu />
           </div>
           <div>
             <Color />
           </div>
-        </div>
-
-        {/* Mobile Button */}
-        <div onClick={handleNav} className="block sm:hidden z-20">
-          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size />}
-        </div>
-        {/* Mobile Menu */}
-        <div
-          className={
-            nav
-              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white z-10 text-center ease-in duration-300"
-              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white z-10 text-center ease-in duration-300"
-          }
-          onClick={() => setNav(false)}
-        >
-          <ul>
-            <li className="p-4 text-3xl">
-              <div
-                className={
-                  router.pathname === "/"
-                    ? "bg-gradient-to-br from-yellow-400 via-orange-400 to-yellow-400 px-2 rounded-md"
-                    : ""
-                }
-              >
-                <Link href="/">Acasa</Link>
-              </div>
-            </li>
-            <li className="p-4 text-3xl">
-              <div
-                className={
-                  router.pathname === "/shop"
-                    ? "bg-gradient-to-br from-yellow-400 via-orange-400 to-yellow-400 px-2 rounded-md"
-                    : ""
-                }
-              >
-                <Link href="/shop">Acasa</Link>
-              </div>
-            </li>
-            <li className="p-4 text-3xl">
-              <div
-                className={
-                  router.pathname === "/about"
-                    ? "bg-gradient-to-br from-yellow-400 via-orange-400 to-yellow-400 px-2 rounded-md"
-                    : ""
-                }
-              >
-                <Link href="/about">Acasa</Link>
-              </div>
-            </li>
-            <li className="p-4 text-3xl">
-              <div
-                className={
-                  router.pathname === "/contacte"
-                    ? "bg-gradient-to-br from-yellow-400 via-orange-400 to-yellow-400 px-2 rounded-md"
-                    : ""
-                }
-              >
-                <Link href="/contacte">Acasa</Link>
-              </div>
-            </li>
-          </ul>
         </div>
       </div>
     </div>
